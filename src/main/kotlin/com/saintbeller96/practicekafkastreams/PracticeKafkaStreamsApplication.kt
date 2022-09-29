143661e15f836687f4894a5b33f5256eba91b818
rizzle.js <rizzle.js@kakaoent.com>
2022-09-29 16:01:00 +0900

    init

---
+++ src/main/kotlin/com/saintbeller96/practicekafkastreams/PracticeKafkaStreamsApplication.kt
@@ -0,0 +1,11 @@
+package com.saintbeller96.practicekafkastreams
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeKafkaStreamsApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracticeKafkaStreamsApplication>(*args)
+}
