dc7d16a0a46b80b5273dd954045a435bdc82e9ea
rlawhdtjd9 <rlawhdtjd9@naver.com>
2022-10-04 16:15:53 +0900

    스트림 단계별 주석 추가

--- word-count/src/main/kotlin/WordCountApp.kt
+++ word-count/src/main/kotlin/WordCountApp.kt
@@ -4,30 +4,32 @@ import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.StreamsConfig
 import org.apache.kafka.streams.kstream.KStream
 import org.apache.kafka.streams.kstream.Materialized
-import org.apache.kafka.streams.kstream.Named
 import org.apache.kafka.streams.kstream.Produced
 
-class WordCountApp {
-}
-
 fun main() {
-    val config = StreamsConfig(mapOf(
-        StreamsConfig.APPLICATION_ID_CONFIG to "wordcount-application",
-        StreamsConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
-        StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG to Serdes.String().javaClass,
-        StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG to Serdes.String().javaClass,
-    ))
+    val config = StreamsConfig(
+        mapOf(
+            StreamsConfig.APPLICATION_ID_CONFIG to "wordcount-application",
+            StreamsConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+            StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG to Serdes.String().javaClass,
+            StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG to Serdes.String().javaClass,
+        )
+    )
 
     val builder = StreamsBuilder()
 
     val wordCountInput: KStream<String, String> = builder.stream("word-count-input")
 
     wordCountInput.mapValues(String::lowercase)
+        //record = (null, "hello world hello kafka")
         .flatMapValues { text -> text.split(" ") }
+        //(null, "hello"), (null, "world"), (null, "hello"), (null, "kafka")
         .selectKey { _, word -> word }
+        //("hello", "hello"), ("world", "world"), ("hello", "hello"), ("kafka", "kafka")
         .groupByKey()
-        //
+        //("hello", [("hello", "hello"), ("hello", "hello")]), ("world", [("world", "world")]), ("kafka", [("kafka", "kafka")]
         .count(Materialized.`as`("counts"))
+        //("hello": 2), ("world":1), ("kafka":1)
         .toStream()
         .to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()))
 
